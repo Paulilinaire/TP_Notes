4f0a6a42aa294264db3b35041f0637c311a6448e
Pauline <pauline@MacBook-Air-de-Tom.local>
2023-09-22 14:16:08 +0200

    update addGradeButtonFunction

---
+++ modules/js/Grade.js
@@ -0,0 +1,8 @@
+export class Grade {
+    constructor(studentName, studentFirstName, subject, grade){
+        this.studentName = studentName
+        this.studentFirstName = studentFirstName
+        this.subject = subject
+        this.grade = grade
+    }
+}
--- modules/main.js
+++ modules/main.js
@@ -2,11 +2,11 @@ import { Student } from "./js/Student.js"
 import { StudentsList } from "./js/StudentsList.js"
 import { Subject } from "./js/Subject.js"
 import { SubjectsList } from "./js/SubjectsList.js"
+import { Grade } from "./js/Grade.js"
 
 let studentsList = new StudentsList ()
 let subjectsList = new SubjectsList ()
-let subject = document.getElementById("subject")
-let grade = document.getElementById("grade")
+let gradeTable = []
 let studentsClass = document.getElementById("studentsClass")
 let subjectsClass = document.getElementById("subjectsClass")
 
@@ -19,9 +19,9 @@ document.getElementById('addStudentButton').addEventListener('click', addStudent
         let studentFirstName = document.getElementById("studentFirstName").value
         let student = new Student (studentName, studentFirstName)        
         studentsList.addStudent(student)
-        // create a function that get this.students, browse the table and this function will update the select in the DOM
+        // create a function that get this.students[], which browse the table and this function will update the select in the DOM
         console.log(studentsList);
-        //adding student in DOM (select)
+        //adding student in DOM select
         let option = document.createElement("option")
         option.textContent = `${studentName} ${studentFirstName}`
         studentsClass.appendChild(option)
@@ -35,7 +35,7 @@ document.getElementById('addSubjectButton').addEventListener('click', addSubject
         let subjectName = document.getElementById("subjectName").value
         let subject = new Subject (subjectName)
         subjectsList.addSubject(subject)      
-        // create a function that get this.subjects, browse the table and this function will update the select in the DOM
+        // create a function that get this.subjects[], which browse the table and this function will update the select in the DOM
         console.log(subjectsList);
         //adding subject in DOM (select)
         let option = document.createElement("option")
@@ -43,3 +43,27 @@ document.getElementById('addSubjectButton').addEventListener('click', addSubject
         subjectsClass.appendChild(option)
         console.log(subjectsClass);
     }
+
+//Function addGradeButton
+document.getElementById('addGradeButton').addEventListener('click', addGrade)
+    function addGrade(){
+        let studentName2 = document.getElementById("studentName2").value
+        let studentFirstName2 = document.getElementById("studentFirstName2").value
+        let subject = document.getElementById("subject").value
+        let grade = document.getElementById("grade").value
+        let newGrade = new Grade (studentName2, studentFirstName2, subject, grade)
+        gradeTable.push(newGrade)
+        console.log(gradeTable);
+        // Display in the DOM table
+        for(let gradeNumber of gradeTable){
+            table.innerHTML += 
+            `<tr>
+                <td>${studentName2}</td>
+                <td>${studentFirstName2}</td>
+                <td>${subject}</td>
+                <td>${grade}</td>
+            </tr>
+            `
+        }
+        gradeTable = []
+}
